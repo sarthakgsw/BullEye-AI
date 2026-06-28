c4ca98aa12367de720287dcb5f948561a13467d4
Sarthak <sarthakgsw143@gmail.com>
2026-06-28 14:15:25 +0530

    Added requirements and gitignore

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 print("Welcome to BullEye AI 🚀")
 print("Indian Stock Market AI Assistant")
 print("Developer: Sarthak Goswami")
+import yfinance as yf
+
+print("📈 BullEye AI - Market Data Module")
+
+# NSE Stock Symbol
+stock = "RELIANCE.NS"
+
+# છેલ્લા 1 વર્ષનો data download કરો
+data = yf.download(stock, period="1y")
+
+print("\nFirst 5 Rows:\n")
+print(data.head())
+
+print("\nLast 5 Rows:\n")
+print(data.tail())
